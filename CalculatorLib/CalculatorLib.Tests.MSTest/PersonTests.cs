38b96998b3e4441b1865250b1a29ace71fc016e7
Michael Zöhling <michael.zoehling@outlook.com>
2019-12-12 11:59:30 +0100

    Übung: Equals mit Person

---
+++ CalculatorLib/CalculatorLib.Tests.MSTest/PersonTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLib.Tests.MSTest
+{
+    [TestClass]
+    public class PersonTests
+    {
+        // Snippets:
+        // testc + TAB + TAB
+        // testm + TAB + TAB
+
+        [TestMethod]
+        public void Person_Equals_Compare_with_null_returns_false()
+        {
+            Person p1 = new Person { Vorname = "Tom", Nachname = "Ate", Alter = 10, Kontostand = 100 };
+
+            Assert.IsFalse(p1.Equals(null));
+        }
+
+        [TestMethod]
+        public void Person_Equals_Compare_with_same_Reference_returns_true()
+        {
+            Person p1 = new Person { Vorname = "Tom", Nachname = "Ate", Alter = 10, Kontostand = 100 };
+            Person p2 = p1;
+
+            Assert.IsTrue(p1.Equals(p2));
+        }
+
+
+        [TestMethod]
+        public void Person_Equals_Compare_with_same_Values_returns_true()
+        {
+            Person p1 = new Person { Vorname = "Tom", Nachname = "Ate", Alter = 10, Kontostand = 100 };
+            Person p2 = new Person { Vorname = "Tom", Nachname = "Ate", Alter = 10, Kontostand = 100 };
+
+            Assert.IsTrue(p1.Equals(p2));
+        }
+
+        [TestMethod]
+        public void Person_Equals_Compare_with_different_Values_returns_false()
+        {
+            Person p1 = new Person { Vorname = "Tom", Nachname = "Ate", Alter = 10, Kontostand = 100 };
+            Person p2 = new Person { Vorname = "Anna", Nachname = "Nass", Alter = 10, Kontostand = 100 };
+
+            Assert.IsFalse(p1.Equals(p2));
+        }
+
+        [TestMethod]
+        public void Person_Equals_Compare_with_different_Type_returns_false()
+        {
+            Person p1 = new Person { Vorname = "Tom", Nachname = "Ate", Alter = 10, Kontostand = 100 };
+            StringBuilder o2 = new StringBuilder();
+
+            Assert.IsFalse(p1.Equals(o2));
+        }
+    }
+
+}
---
+++ CalculatorLib/CalculatorLib/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLib
+{
+    public class Person
+    {
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public byte Alter { get; set; }
+        public decimal Kontostand { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null || (obj is Person) == false) // null oder falscher Typ -> False
+                return false;
+
+            Person second = (Person)obj;
+            if (this == obj) // Referenzgleich -> True
+                return true;
+
+            else if (this.Vorname == second.Vorname &&
+                    this.Nachname == second.Nachname &&
+                    this.Alter == second.Alter &&
+                    this.Kontostand == second.Kontostand) // Wertegleich
+                return true;
+            else
+                return false;
+        }
+    }
+}
