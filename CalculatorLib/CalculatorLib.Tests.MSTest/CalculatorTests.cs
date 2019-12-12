7d3654f86d454a906efcd923aa75b87fb4c880e1
Michael Zöhling <michael.zoehling@outlook.com>
2019-12-12 10:00:40 +0100

    Unser erster Unittest

---
+++ CalculatorLib/CalculatorLib.Tests.MSTest/CalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorLib.Tests.MSTest
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void Add_3_and_5_returns_8()
+        {
+            // Arrange:
+            Calculator c = new Calculator();
+
+            // Action:
+            int result = c.Add(3, 5);
+
+            // Assert:
+            Assert.AreEqual(8, result);
+        }
+    }
+}
