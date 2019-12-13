9d7581fb51977f2bdab41901353280ea65ee0200
Michael Zöhling <michael.zoehling@outlook.com>
2019-12-13 11:12:26 +0100

    EFRepository erstellt

---
+++ ProjectYeong/ppedv.ProjectYeong.Data.EF/EFRepository.cs
@@ -0,0 +1,61 @@
+﻿using ppedv.ProjectYeong.Domain;
+using ppedv.ProjectYeong.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ppedv.ProjectYeong.Data.EF
+{
+    public class EFRepository : IRepository
+    {
+        public EFRepository(EFContext context)
+        {
+            this.context = context;
+        }
+        private readonly EFContext context;
+
+
+
+        public void Add<T>(T item) where T : Entity
+        {
+            context.Set<T>().Add(item);
+        }
+
+        public void Delete<T>(T item) where T : Entity
+        {
+            context.Set<T>().Remove(item);
+        }
+
+        public IEnumerable<T> GetAll<T>() where T : Entity
+        {
+            return context.Set<T>().ToArray();
+        }
+
+        public T GetByID<T>(int ID) where T : Entity
+        {
+           return context.Set<T>().Find(ID);
+        }
+
+        public IQueryable<T> Query<T>() where T : Entity
+        {
+            return context.Set<T>();
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public void Update<T>(T item) where T : Entity
+        {
+            var loadedItem = GetByID<T>(item.ID);
+            if(loadedItem != null)
+            {
+                context.Entry(loadedItem).CurrentValues.SetValues(item);
+                // Setzt alle Werte auf die Werte, die in "item" drinnenstehen
+                // Changetracker erkennt die wertänderungen -> "Update"-Befehl ;)
+            }
+        }
+    }
+}
