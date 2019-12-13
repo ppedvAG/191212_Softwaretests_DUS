7367dd3b9312a86fbfbb961512568c006e1f86b5
Michael Zöhling <michael.zoehling@outlook.com>
2019-12-13 11:06:09 +0100

    Interface für das Repository-Pattern

---
+++ ProjectYeong/ppedv.ProjectYeong.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ppedv.ProjectYeong.Domain.Interfaces
+{
+    public interface IRepository
+    {
+        void Add<T>(T item) where T : Entity;
+        void Delete<T>(T item) where T : Entity;
+        void Update<T>(T item) where T : Entity;
+        T GetByID<T>(int ID) where T : Entity;
+        IEnumerable<T> GetAll<T>() where T : Entity;
+        IQueryable<T> Query<T>() where T : Entity;
+        void Save();
+
+    }
+}
