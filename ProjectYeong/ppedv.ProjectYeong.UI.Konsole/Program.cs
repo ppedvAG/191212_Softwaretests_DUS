ae3562025722b8ff6788537e87a8838e2d88cb4c
Michael Zöhling <michael.zoehling@outlook.com>
2019-12-13 11:18:20 +0100

    Init Konsole

---
+++ ProjectYeong/ppedv.ProjectYeong.UI.Konsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.ProjectYeong.UI.Konsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
