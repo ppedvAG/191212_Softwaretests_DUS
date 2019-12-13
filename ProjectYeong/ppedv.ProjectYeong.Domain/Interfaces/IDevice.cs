22d4e45ad049ec5bbcfd8ecccd8d78b1c6bf86c5
Michael Zöhling <michael.zoehling@outlook.com>
2019-12-13 11:31:48 +0100

    Interface für Hardware erstellt

---
+++ ProjectYeong/ppedv.ProjectYeong.Domain/Interfaces/IDevice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ppedv.ProjectYeong.Domain.Interfaces
+{
+    public interface IDevice
+    {
+        Book PrintBook();
+    }
+}
