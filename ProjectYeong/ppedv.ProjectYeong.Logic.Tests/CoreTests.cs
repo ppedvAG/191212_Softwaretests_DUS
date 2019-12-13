b817beabd6e9755ef165ecfc32e9a809734bbc33
Michael Zöhling <michael.zoehling@outlook.com>
2019-12-13 11:54:36 +0100

    Ausgangssituation: Echte Maschine wird im "UnitTest" angesteuert

---
+++ ProjectYeong/ppedv.ProjectYeong.Logic.Tests/CoreTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Robotech.Hardware;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.ProjectYeong.Logic.Tests
+{
+    [TestFixture]
+    public class CoreTests
+    {
+        [Test]
+        public void Core_can_print_5_Books()
+        {
+            Core core = new Core(new BookPrinter3000());
+
+            var result = core.PrintBooks(5);
+
+            result.Should().HaveCount(5);
+        }
+    }
+}
