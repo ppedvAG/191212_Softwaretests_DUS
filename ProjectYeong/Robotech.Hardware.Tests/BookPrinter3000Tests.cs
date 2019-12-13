6d5f61fff6fef6dd7e1e38ccd786ad5bc38cab5b
Michael Zöhling <michael.zoehling@outlook.com>
2019-12-13 11:40:35 +0100

    Hardware getestet (UnitTest)

---
+++ ProjectYeong/Robotech.Hardware.Tests/BookPrinter3000Tests.cs
@@ -0,0 +1,23 @@
+﻿using ppedv.ProjectYeong.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Robotech.Hardware.Tests
+{
+    public class BookPrinter3000Tests
+    {
+        [Fact]
+        public void BookPrinter3000_can_print_a_Book()
+        {
+            BookPrinter3000 bp = new BookPrinter3000();
+
+            Book result = bp.PrintBook();
+
+            Assert.NotNull(result);
+        }
+    }
+}
