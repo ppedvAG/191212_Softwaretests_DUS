e1fc52160d54d3c6a9f09306a6125adbb7746d72
Michael Zöhling <michael.zoehling@outlook.com>
2019-12-13 11:17:40 +0100

    Sehr simple Logik

---
+++ ProjectYeong/ppedv.ProjectYeong.Logic/Core.cs
@@ -0,0 +1,34 @@
+﻿using AutoFixture;
+using ppedv.ProjectYeong.Domain;
+using ppedv.ProjectYeong.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ppedv.ProjectYeong.Logic
+{
+    public class Core
+    {
+        public Core(IRepository repo)
+        {
+            this.repo = repo;
+        }
+        private readonly IRepository repo;
+
+        // Geschäftslogik
+
+        public void GenerateTestData()
+        {
+            Fixture fix = new Fixture();
+
+            var stores = fix.CreateMany<BookStore>(5);
+
+            foreach (var store in stores)
+            {
+                repo.Add(store);
+            }
+
+            repo.Save();
+        }
+    }
+}
